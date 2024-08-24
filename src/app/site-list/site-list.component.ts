ca1ba419b5b91b4d5a4f1233a6b375e657c3d261
shubhujais15 <shubhujaisedu15@gmail.com>
2024-08-24 17:07:43 +0530

    form submit & site load through service injection

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { PasswordManagerService } from '../services/password-manager.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css'
 })
 export class SiteListComponent {
+
+  
+
+  constructor(private passwordManager: PasswordManagerService){
+    this.loadSites();
+  }
+
+  onSubmit(values: object){
+    // console.log(values);
+    this.passwordManager.addSite(values)
+    .then(()=>{
+      console.log("Data Save Successfully");
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+
+  loadSites(){
+    this.passwordManager.loadSites().subscribe((val:any)=>{
+      console.log(val);
+    })
+  }
 
 }
