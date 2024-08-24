3f7512a21e21dfbd4d9ce8bb6d97958db515094c
shubhujais15 <shubhujaisedu15@gmail.com>
2024-08-24 18:39:16 +0530

    added add,edit,delete func & also alert msg

--- src/app/site-list/site-list.component.ts
+++ src/app/site-list/site-list.component.ts
@@ -2,36 +2,82 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { PasswordManagerService } from '../services/password-manager.service';
 import { Observable } from 'rxjs';
+import { AsyncPipe, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-site-list',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule, AsyncPipe],
   templateUrl: './site-list.component.html',
   styleUrl: './site-list.component.css'
 })
 export class SiteListComponent {
 
-  
+  allSites: Observable<Array<any>> = new Observable<Array<any>>();  
+
+  siteName: string = "";
+  siteURL: string = "";
+  siteImgURL: string = "";
+  siteId!: string;
+
+  formState: string = "Add New";
+
+  isSuccess: boolean = false;
+
+  successMessage: string="";
 
   constructor(private passwordManager: PasswordManagerService){
     this.loadSites();
   }
 
+  showAlert(message: string){
+    this.isSuccess = true;
+    this.successMessage = message;
+  }
+
   onSubmit(values: object){
     // console.log(values);
+    if(this.formState == "Add New"){
     this.passwordManager.addSite(values)
     .then(()=>{
-      console.log("Data Save Successfully");
+      this.showAlert('Data Saved Successfully');
     })
     .catch((error)=>{
       console.log(error);
     })
   }
+  else if(this.formState == "Edit"){
+    this.passwordManager.updateSite(this.siteId,values)
+    .then(()=>{
+      this.showAlert('Data Edited Successfully');
+    })
+    .catch((error)=>{
+      console.log(error);
+    })
+  }
+  }
+ 
 
   loadSites(){
-    this.passwordManager.loadSites().subscribe((val:any)=>{
-      console.log(val);
+    this.allSites = this.passwordManager.loadSites();
+  }
+
+  editSite(siteName: string, siteURL: string, siteImgURL: string, id:string){
+    this.siteName = siteName;
+    this.siteURL = siteURL;
+    this.siteImgURL = siteImgURL;
+    this.siteId = id;
+    
+    this.formState = "Edit";
+  }
+
+  deleteSite(id: string){
+    this.passwordManager.deleteSite(id)
+    .then(()=>{
+      this.showAlert('Data Deleted Successfully');
+    })
+    .catch((error)=>{
+      console.log(error);
     })
   }
 
