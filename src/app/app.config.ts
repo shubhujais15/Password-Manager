b60e7004ab13e65796419cab1770495dc099e9c7
shubhujais15 <shubhujaisedu15@gmail.com>
2024-08-24 15:46:15 +0530

    Initialize Angular,Tailwind & Firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"password-manager-de6ca","appId":"1:911209794863:web:55b65569f915f1583465da","storageBucket":"password-manager-de6ca.appspot.com","apiKey":"AIzaSyC_HEBxDT_dFqLBHmoNYyi_0q7KHtjxc_M","authDomain":"password-manager-de6ca.firebaseapp.com","messagingSenderId":"911209794863"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
---
+++ src/app/site-list/site-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-site-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './site-list.component.html',
+  styleUrl: './site-list.component.css'
+})
+export class SiteListComponent {
+
+}
