25c747cb17e7ca6344ada4032ac3bacb75c3f0b6
shubhujais15 <shubhujaisedu15@gmail.com>
2024-08-26 18:14:15 +0530

    navbar button hidden func

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  showNav = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        // console.log('NavigationEnd:', event.urlAfterRedirects);
+        this.showNav = event.urlAfterRedirects !== '/';
+        // console.log('Show Nav:', this.showNav);
+      });
+  }
 
 }
