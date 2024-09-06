a15c32fb16047a970989bf1d64618f6d91634333
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-06 20:42:12 +0530

    initialize routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,20 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SiteListComponent } from './site-list/site-list.component';
+import { PasswordListComponent } from './password-list/password-list.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
-        path:" ",
-        
-    }
+        path:"",
+        component:LoginComponent
+    },
+    {
+        path:"site-list",
+        component:SiteListComponent
+    },
+    {
+        path:"password-list",
+        component:PasswordListComponent
+    },
 ];
