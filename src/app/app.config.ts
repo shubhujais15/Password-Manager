7097584149f4f4834d0ae481df0ba23835347331
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-06 23:04:18 +0530

    auth setting

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,26 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment'; // <-- Import environment
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"password-manager-de6ca","appId":"1:911209794863:web:55b65569f915f1583465da","storageBucket":"password-manager-de6ca.appspot.com","apiKey":"AIzaSyC_HEBxDT_dFqLBHmoNYyi_0q7KHtjxc_M","authDomain":"password-manager-de6ca.firebaseapp.com","messagingSenderId":"911209794863"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'password-manager-de6ca',
+        appId: '1:911209794863:web:55b65569f915f1583465da',
+        storageBucket: 'password-manager-de6ca.appspot.com',
+        apiKey: environment.firebaseConfig.apiKey,  // <-- Use the API key from environment
+        authDomain: 'password-manager-de6ca.firebaseapp.com',
+        messagingSenderId: '911209794863',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
     production: false,
-}
+    firebaseConfig: {
+      apiKey: 'AIzaSyC_HEBxDT_dFqLBHmoNYyi_0q7KHtjxc_M', // Replace with the actual development API key if different
+      authDomain: 'password-manager-de6ca.firebaseapp.com',
+      projectId: 'password-manager-de6ca',
+      storageBucket: 'password-manager-de6ca.appspot.com',
+      messagingSenderId: '911209794863',
+      appId: '1:911209794863:web:55b65569f915f1583465da'
+    }
+  };
