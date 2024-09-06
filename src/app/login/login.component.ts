b287626ace9c626aa269b9759ac90170ab6fe33a
shubhujais15 <shubhujaisedu15@gmail.com>
2024-09-06 20:41:45 +0530

    login page with firebase auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
-
+import { PasswordManagerService } from '../services/password-manager.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+  isError: boolean = false;
+
+  constructor(private passwordManager: PasswordManagerService, private router: Router){
+  }
+
+  onSubmit(values:any){
+    this.passwordManager.login(values.email, values.password)
+    .then(()=>{
+      console.log('Login success')
+      this.router.navigate(['/site-list'])
+    })
+    .catch((error)=>{
+      console.log('Login failed')
+      this.isError = true;
+    })
+  }
 
 }
